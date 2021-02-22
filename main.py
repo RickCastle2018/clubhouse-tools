54d7c727d413a8fbc6ca2286cbda3b47a6ebf4e6
Nikita <nnikonov2006@gmail.com>
2021-02-22 21:32:41 +0300

    initital commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from clubhouse.clubhouse import Clubhouse
+
+if __name__ == "__main__":
+    clubhouse = Clubhouse()
+
+    #hello message
+    print("clubhouse-invite \n©2021 by Nikita Nikonov")
+
+    #user enters tel
+    phone_number = input("Enter your phone number to login into Clubhouse: ")
+
+    #send verification code
+    clubhouse.start_phone_number_auth(phone_number)
+    print("Verification code sent")
+
+    #user enters sms verification code to login
+    verification_code = input("Enter it: ")
+    clubhouse.complete_phone_number_auth(phone_number, verification_code)
+
+    #user enters phone number of user he wants to invite
+    phone_number = input("Enter phone number of user you want to invite: ")
+    name = input("Enter name of user you want to invite: ")
+    clubhouse.invite_to_app(name, phone_number, message="Invited from clubhouse-invite")
